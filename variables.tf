f5e11381776f225a727f480e1884a8bc4bf65671
bonzovt <adamfisher.it@gmail.com>
2021-07-15 20:45:15 +0000

    added s3 bucket v4

--- variables.tf
+++ variables.tf
@@ -49,4 +49,4 @@ variable "placeholder" {
   description = "Image-as-a-service URL. Some other fun ones to try are fillmurray.com, placecage.com, placebeard.it, loremflickr.com, baconmockup.com, placeimg.com, placebear.com, placeskull.com, stevensegallery.com, placedog.net"
 }
 
-variable "s3_bucket_acceleration_status" { default = "suspended" }
+variable "s3_bucket_acceleration_status" { default = "Suspended" }
